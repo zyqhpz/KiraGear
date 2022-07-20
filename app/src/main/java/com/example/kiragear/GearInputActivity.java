bc7366a046afa3a77c468fe2f52c52a62c331bc0
Haziq Hapiz <zyqq.dev@gmail.com>
2022-07-20 19:18:53 +0800

    add new activity

---
+++ app/src/main/java/com/example/kiragear/GearInputActivity.java
@@ -0,0 +1,14 @@
+package com.example.kiragear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GearInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gear_input);
+    }
+}
