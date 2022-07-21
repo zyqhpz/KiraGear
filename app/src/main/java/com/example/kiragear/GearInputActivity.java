8278188d42b3de9c58c1593519386be4573a5edb
Haziq Hapiz <zyqq.dev@gmail.com>
2022-07-21 14:06:55 +0800

    add text input and button

--- app/src/main/java/com/example/kiragear/GearInputActivity.java
+++ app/src/main/java/com/example/kiragear/GearInputActivity.java
@@ -2,7 +2,12 @@ package com.example.kiragear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class GearInputActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class GearInputActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gear_input);
+
+        TextView tireWidthLabel = (TextView) findViewById(R.id.tireWidthText);
+        tireWidthLabel.setTextColor(Color.BLACK);
+
+        TextView tireAspectRatioLabel = (TextView) findViewById(R.id.tireAspectRatioText);
+        tireAspectRatioLabel.setTextColor(Color.BLACK);
+
+        TextView tireDiameterLabel = (TextView) findViewById(R.id.tireDiameterText);
+        tireDiameterLabel.setTextColor(Color.BLACK);
+
+
+
+
     }
 }
